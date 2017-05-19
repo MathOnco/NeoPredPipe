b8093c70b3267af84631de4b2a2c921bb810247d
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2017-05-19 15:56:28 -0400

    Test Files

--- main_netMHCpan_pipe.py
+++ main_netMHCpan_pipe.py
@@ -7,21 +7,32 @@ import ConfigParser
 
 # get user variables
 parser = argparse.ArgumentParser()
-parser.add_argument("I", type=str, help="Input vcf files location")
-parser.add_argument("E", type=str, help="Comma separated epitope lengths for prediction (e.g. 8,9,10)")
-parser.add_argument("H", type=str, help="Polysolver HLA Type file")
-parser.add_argument("O", type=str, help="Output Directory Path")
+parser.add_argument("InputCSV", type=str, help=".csv file with Input VCF, normal BAM, semi-colon separated epitope lengths, Polysolver HLA Type File")
+##parser.add_argument("I", type=str, help="Input vcf files location")
+##parser.add_argument("E", type=str, help="Comma separated epitope lengths for prediction (e.g. 8,9,10)")
+##parser.add_argument("H", type=str, help="Polysolver HLA Type file")
+parser.add_argument("OutputDir", type=str, help="Output Directory Path")
 args = parser.parse_args() # main user args
-localpath = os.path.abspath(__file__).rstrip('main_netMHCpan_pipe.py') # path to scripts working directory
 
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                print("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
 
-class MyParser(ConfigParser.ConfigParser):
+if __name__=="__main__":
+    # Pull information about usr system files
+    localpath = os.path.abspath(__file__).rstrip('main_netMHCpan_pipe.py')  # path to scripts working directory
+    Config = ConfigParser.ConfigParser()
+    Config.read(localpath + "usr_paths.ini")
+    annPaths =ConfigSectionMap(Config.sections()[0]) # get annovar script paths
 
-    def as_dict(self):
-        d = dict(self._sections)
-        for k in d:
-            d[k] = dict(self._defaults, **d[k])
-            d[k].pop('__name__', None)
-        return d
+    print annPaths
 
-MyParser.as_dict()
