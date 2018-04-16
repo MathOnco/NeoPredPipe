e8dd967334d7b3dc09194628493d7af7c075be80
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2018-04-16 12:34:03 +0100

    Fixed filepath error for filtered neoantigens

--- StandardPredsClass.py
+++ StandardPredsClass.py
@@ -68,8 +68,8 @@ class StandardPreds:
             if ba <= 500.0:
                 dataOut.append('\t'.join(line))
 
-        if os.path.isfile(self.OutDir + self.filename.replace('.unfiltered.txt','.filtered.txt'))==False:
-            with open(self.OutDir + self.filename.replace('.unfiltered.txt','.filtered.txt'), 'w') as outFile:
+        if os.path.isfile(self.filename.replace('.unfiltered.txt','.filtered.txt'))==False:
+            with open(self.filename.replace('.unfiltered.txt','.filtered.txt'), 'w') as outFile:
                 outFile.write('\n'.join(dataOut))
         else:
             pass
