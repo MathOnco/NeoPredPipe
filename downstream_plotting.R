6c79672d58505b70aedd7760906c43eba342b595
elakatos <e.lakatos@qmul.ac.uk>
2018-10-05 15:46:41 +0100

    Updated plotting for 5-region Venn diagram and boxplot RecoPo

--- downstream_plotting.R
+++ downstream_plotting.R
@@ -32,26 +32,28 @@ strength.df <- data.frame(Binder=c('Total', 'SB', 'WB'),
 strength.mdf <- melt(strength.df, id='Binder')
 p.str <- ggplot(strength.mdf, aes(x=Binder, y=value, fill=variable)) + geom_bar(stat='identity', position='fill') +
   scale_y_continuous(labels=percent_format()) + scale_x_discrete(limits=c('WB', 'SB', 'Total')) +
-  coord_flip() + scale_fill_manual(values=c('#4e9ed0','#dba002','#ce5c55')) +
+  coord_flip() + scale_fill_manual(values=c('#ce5c55','#dba002','#4e9ed0')) +
   labs(y='Proportion of neoepitopes', fill='Clonality') + theme_bw() + theme(text=element_text(size=14))
   
 #Neoepitopes in regions
 
+numRegionsVenn <- 5
+
 region_eps = list()
-for (i in 1:numRegions){
+for (i in 1:numRegionsVenn){
   region_eps[[i]] <- row.names(epTable[epTable[,paste0('Region',i)]==1,])
 }
 
 venn.diagram(x = region_eps,
-             category.names = paste0('Region',1:numRegions),
+             category.names = paste0('Region',1:numRegionsVenn),
              filename=paste0(sampleName,'.epitopes_regions.tiff'),
              imagetype='tiff',
              units='in',
-             resolution=1200,
-             height=3.5,
-             width=4.5,
-             fill=colors[1:numRegions],
-             cex=1.25, cat.cex=1.25)
+             resolution=3000,
+             height=4,
+             width=5.5,
+             fill=colors[1:numRegionsVenn],
+             cex=1.1, cat.cex=1.1, margin=0.1)
 
 #Recognition potential in regions
 recopoTable.filtered <- subset(recopoTable, NeoantigenRecognitionPotential>1e-4)
@@ -59,17 +61,18 @@ epTable.filtered <- subset(epTable, peptide %in% recopoTable.filtered$MutantPept
 
 epTable.filtered$RecognitionPotential <- recopoTable.filtered$NeoantigenRecognitionPotential[match(epTable.filtered$peptide,
                                                                                                    recopoTable.filtered$MutantPeptide)]
-recopo.df <- data.frame(Region1 = epTable.filtered$Region1*epTable.filtered$RecognitionPotential,
-                        Region2 = epTable.filtered$Region2*epTable.filtered$RecognitionPotential,
-                        Region3 = epTable.filtered$Region3*epTable.filtered$RecognitionPotential,
-                        Region4 = epTable.filtered$Region4*epTable.filtered$RecognitionPotential)
-recopo.mdf <- subset(melt(recopo.df), value>0)
+recopo.df <- data.frame(Region1 = epTable.filtered$Region1*epTable.filtered$RecognitionPotential)
+for (i in 2:numRegions){
+  recopo.df[,paste0('Region',i)] <- epTable.filtered[,paste0('Region',i)]*epTable.filtered$RecognitionPotential
+}
+recopo.mdf <- melt(recopo.df)
+recopo.mdf <- subset(recopo.mdf, value > 1e-4)
 
-p.recopo <- ggplot(recopo.mdf, aes(x=variable, y=value,fill=variable)) + geom_violin(alpha=0.7) + geom_point() +
+p.recopo <- ggplot(recopo.mdf, aes(x=variable, y=value,fill=variable)) + geom_boxplot(alpha=0.7) + geom_jitter(width=0.1, height=0) +
   scale_fill_manual(values=colors) +
   labs(x='', y='Recognition potential') + guides(fill=F) + theme_bw() + theme(text=element_text(size=14))
 
 #Save to files
-pdf(paste0(sampleName,'.regions.pdf'), height=3, width=4.5);print(p.reg); dev.off()
+pdf(paste0(sampleName,'.regions.pdf'), height=3, width=5);print(p.reg); dev.off()
 pdf(paste0(sampleName,'.strength.pdf'), height=3, width=5);print(p.str); dev.off()
-pdf(paste0(sampleName,'.recopo_regions.pdf'), height=3.5, width=5);print(p.recopo); dev.off()
+pdf(paste0(sampleName,'.recopo_regions.pdf'), height=3.5, width=5);print(p.recopo + scale_y_log10()); dev.off()
