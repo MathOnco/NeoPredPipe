f1f4dc0221c7dc63c85c54bc276f874fe772fbff
elakatos <e.lakatos@qmul.ac.uk>
2019-01-10 17:40:23 +0000

    Streamlined peptide matching input/algorithm.

--- postprocessing.py
+++ postprocessing.py
@@ -30,7 +30,7 @@ def DigestIndSample(toDigest, patName, checkPeptides, pepmatchPaths, indels=Fals
         pmInputFile = 'tmp/'+patName+'.epitopes.peptidematch.input'
         pmOutFile = 'tmp/'+patName+'.epitopes.peptidematch.out'
 
-    pmInput = open(pmInputFile,'w')
+    pmInputDict = {}
     pmInputLines = 0
     for epFile in toDigest:
         print("INFO: Digesting neoantigens for %s" % (patName))
@@ -42,13 +42,14 @@ def DigestIndSample(toDigest, patName, checkPeptides, pepmatchPaths, indels=Fals
                         linespl = line.split()
                         lines.append('\t'.join(linespl))
                         if checkPeptides:
-                            pmInput.write('>' + linespl[10] + ';' + linespl[2] + '\n' + linespl[2] + '\n')
+                            pep = linespl[2]
+                            pmInputDict['>'+pep] = pep
                             pmInputLines+=1
                 except IndexError as e:
                     pass
-    pmInput.close()
+                    
     if checkPeptides and pmInputLines>0:
-        RunPepmatch(pmInputFile, pepmatchPaths['peptidematch_jar'], pepmatchPaths['reference_index'], pmOutFile)
+        RunPepmatch(pmInputDict,pmInputFile, pepmatchPaths['peptidematch_jar'], pepmatchPaths['reference_index'], pmOutFile)
         lines = ProcessPepmatch(pmOutFile, lines)
     print("INFO: Object size of neoantigens: %s Kb"%(sys.getsizeof(lines)))
     return(lines)
@@ -179,7 +180,11 @@ def AppendDigestedEps(digestedEps, patName, exonicVars, avReady, Options):
     return(newLines, genoTypesPresent)
 
 
-def RunPepmatch(pmInput, pepmatchJar, refIndex, pmfileName):
+def RunPepmatch(pmInputDict, pmInput, pepmatchJar, refIndex, pmfileName):
+    with open(pmInput, 'w') as inputFile:
+        for k in pmInputDict.keys():
+            inputFile.write(k+'\n'+pmInputDict[k]+'\n')
+
     with open('logForPeptideMatch.tmp', 'a') as logFile:
         cmd = ['java', '-jar', pepmatchJar, '-a', 'query', '-i', refIndex,'-Q', pmInput, '-o', pmfileName]
         runcmd = subprocess.Popen(cmd, stdout=logFile)
@@ -192,7 +197,7 @@ def ProcessPepmatch(pmfileName, epLines):
         pmDict = {line.split('\t')[0] : line.split('\t')[1].rstrip('\n') for line in pmFile.readlines() }
     appendedLines = []
     for line in epLines:
-        epkey = line.split('\t')[10]+';'+line.split('\t')[2]
+        epkey = line.split('\t')[2]
         novel = int(pmDict[epkey]=='No match')
         appendedLines.append(line+'\t'+str(novel))
 
